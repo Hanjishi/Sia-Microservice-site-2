7385b77056da0cc3f1bad05be1f3739898d98a5c
Hanjishi <dalisay.john06@gmail.com>
2025-04-13 02:41:03 +0800

    deployment

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/users', ['uses' => 'UserController@getUsers']);
+$router->get('/users', 'UserController@index');//get all users records
+$router->post('/users', 'UserController@add');//create new user record
+$router->get('/users/{id}', 'UserController@show');//get user by id
+$router->put('/users/{id}', 'UserController@update');//update user record
+$router->patch('/users/{id}', 'UserController@update');//update user record
+$router->delete('/users/{id}', 'UserController@delete');//delete record
+
+$router->get('/usersjob', 'UserJobController@index');
+$router->get('/userjob/{id}', 'UserJobController@show'); // get user by id
